abe4c3a6b836beb2961e912d0242c213b66c2157
Bruno <bpires.siqueira@gmail.com>
2024-07-31 21:23:51 -0300

    feat: exercise write files

--- forms_validate_files/exercise2/index.php
+++ forms_validate_files/exercise2/index.php
@@ -1,3 +1,11 @@
+1° Você irá criar uma lista de nomes em um arquivo .txt
+2° No index, você irá ler essa lista do arquivo .txt e exibir essa lista.
+3° Criar um formulário com um input e um botao de envio
+4° O usuario irá digitar um nome na caixa de input e enviar
+5° Ao clicar em enviar, o nome digitado deve ir para essa lista .txt
+6° Exiba uma mensagem de erro se o usuario tentar enviar um nome vazio.
+
+
 <?php
 session_start();
 
