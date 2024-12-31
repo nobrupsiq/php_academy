6551281e1616418afa5921b4c322d3d1432e6fc0
bruno <bpires.siqueira@gmail.com>
2024-12-31 20:09:02 -0300

    descontructing array list

---
+++ array_manipulation/desconstructing_array_list.php
@@ -0,0 +1,8 @@
+<?php
+
+$array = ['Bruno', 90, 'café', 'azul'];
+
+list($nome, $idade, $bebida, $cor) = $array;
+
+
+echo $nome.' tem '.$idade.' anos e gosta de tomar '.$bebida.' olhando o céu '.$cor;
