ba0b995c3e9e9b67df3395c97aa2e0c8a2c6562c
bruno <bpires.siqueira@gmail.com>
2025-01-01 23:01:51 -0300

    metodo construtor

---
+++ object_orientation/constructor_method.php
@@ -0,0 +1,29 @@
+<?php
+class Post {
+  public int $id;
+  public int $likes = 0;
+  public array $comments = [];
+  public string $author;
+
+  public function __construct($postID) { 
+    $this -> id = $postID;
+    // consultar banco de dados para pegar informações do POST
+    $this -> likes = 12;
+  }
+
+  public function aumentarLike() {
+    echo 'abc';
+    $this -> likes++;
+  }
+}
+
+// OBJETO
+$post1 = new Post(1);
+
+$post1 -> comments = ['teste'];
+$post1 -> author = 20; // Ele vai aceitar, pq o PHP consegue entender e transformar o numero em string
+$post2 = new Post(2);
+
+
+echo 'POST 1: '.$post1 -> likes.'</br>';
+echo 'POST 2: '.$post2 -> likes;
