a91062c261e2f480f63eb4525d9d5cc227cc76da
bruno <bpires.siqueira@gmail.com>
2025-01-09 20:41:36 -0300

    PSR-1

---
+++ object_orientation/PSR-1.php
@@ -0,0 +1,16 @@
+<?php
+// 1. Visão geral
+// Os arquivos DEVEM usar apenas as tags <?php e <?=.
+
+// Os arquivos DEVEM usar apenas UTF-8 sem BOM para código PHP.
+
+// Os arquivos DEVEM declarar símbolos (classes, funções, constantes, etc.) ou causar efeitos colaterais (por exemplo, gerar saída, alterar configurações .ini, etc.), mas NÃO DEVEM fazer as duas coisas.
+// - Exemplo, um arquivo cria a classe e o outro arquivo faz as implementações dessa classe, os efeitos colaterais no caso.
+
+// Os namespaces e classes DEVEM seguir um PSR de "carregamento automático": [PSR-0, PSR-4].
+
+// Os nomes de classe DEVEM ser declarados em StudlyCaps.
+
+// As constantes de classe DEVEM ser declaradas em letras maiúsculas com separadores de sublinhado.
+
+// Os nomes de método DEVEM ser declarados em camelCase.
