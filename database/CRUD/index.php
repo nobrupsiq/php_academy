fcafc27edc6372dd1e804a1f8037185b37602588
bruno <bpires.siqueira@gmail.com>
2025-01-12 22:51:33 -0300

    leitura de dados

--- database/CRUD/index.php
+++ database/CRUD/index.php
@@ -6,6 +6,12 @@
 // D -> DELETE
 
 require 'config.php';
+
+$lista = [];
+$sql = $pdo -> query("SELECT * FROM usuarios");
+if($sql -> rowCount() > 0) {
+    $lista = $sql -> fetchAll(PDO::FETCH_ASSOC); // FETCH_ASSOC faz associação de campos(input's)
+}
 ?>
 
 <a href="adicionar.php">ADICIONAR USUARIO</a>
@@ -17,4 +23,15 @@ require 'config.php';
         <th>EMAIL</th>
         <th>AÇÕES</th>
     </tr>
+    <?php foreach($lista as $usuario):  ?>
+        <tr>
+            <td><?= $usuario['id'];?></td>
+            <td><?= $usuario['nome'];?></td>
+            <td><?= $usuario['email'];?></td>
+            <td>
+                <a href="editar.php?id=<?=$usuario['id'];?>">[ EDITAR ]</a>
+                <a href="excluir.php?id=<?=$usuario['id'];?>">[ EXCLUIR ]</a>
+            </td>
+        </tr>
+    <?php endforeach; ?>
 </table>
