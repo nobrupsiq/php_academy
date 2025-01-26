3b137c1c2d4bf4d0d2dc2ef858787ce432a5ecbc
bruno <bpires.siqueira@gmail.com>
2025-01-26 09:30:11 -0300

    Principio Solid letra S

---
+++ database/SOLID/S.php
@@ -0,0 +1,15 @@
+<?php
+
+// Principio da responsabilidade unica
+// Toda classe deve ter um e somente um motivo para mudar. A classe precisa ter apenas uma responsabilidade
+
+class Usuario {
+    public function set_nome(){}
+    public function get_nome() {}
+    
+    public function add() {}
+    public function update() {}
+    public function delete() {}
+
+}
+
---
+++ database/SOLID/introduction.php
@@ -0,0 +1,7 @@
+<?php
+
+// S - Single Responsibility Principe (Principio da responsabilidade unica)
+// O - Open-Closed Princeiple (Principio Aberto-Fechado)
+// L - Liskov Substitution Principe (Principio da substituição de Liskov)
+// I - Interface Segregation Principle (Principio da Segregação da Interface)
+// D - Dependency Inversion Principle (Principio da inversão da Dependência)
